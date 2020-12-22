f9c9d753942a892d135f1507d29b07d6c5fbf54e
airmax11 <maksvellspam@gmail.com>
2020-12-22 22:18:18 +0100

    test

--- Demo/TC1_Checkboxes.py
+++ Demo/TC1_Checkboxes.py
@@ -4,7 +4,7 @@ import time
 driver = webdriver.Chrome(executable_path="c:/webdirver/chromedriver.exe")
 driver.get("https://rahulshettyacademy.com/AutomationPractice/")
 
-time.sleep(2)
+time.sleep(1)
 
 
 lst = driver.find_elements_by_css_selector("input[type='checkbox']")
@@ -14,9 +14,23 @@ print(len(lst))
 
 for i in lst:
     i.click()
+    print(i.get_attribute("value"))
     print(i.is_selected())
     assert i.is_selected()
 
-time.sleep(2)
+
+lst_radio = driver.find_elements_by_css_selector("input[type='radio']")
+lst_radio[2].click()
+
+driver.find_element_by_css_selector("input[id='name']").send_keys("Test123")
+driver.find_element_by_css_selector("input[type='submit'][value='Alert']").click()
+alert = driver.switch_to.alert
+txt = alert.text
+alert.accept()
+
+assert "Test123" in txt
+
+
+time.sleep(1)
 
 driver.close()
