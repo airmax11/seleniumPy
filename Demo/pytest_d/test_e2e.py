2031a5a666cb52f0a141af6094b794734cc56779
airmax11 <maksvellspam@gmail.com>
2021-01-14 16:58:44 +0100

    update

--- Demo/pytest_d/test_e2e.py
+++ Demo/pytest_d/test_e2e.py
@@ -3,6 +3,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import pytest
 # from Demo.imp import prec as dr
+from Demo.pageobject.CheckOutPage import CheckOutPageObject
+from Demo.pageobject.ProductPage import ProductPageObject
 from Demo.utilities.BaseClass import Baseclass
 from Demo.pageobject.HomePage import HomePageObject
 
@@ -11,23 +13,16 @@ from Demo.pageobject.HomePage import HomePageObject
 class Testsuit_01(Baseclass):
 
     def test_e2e(self):
-        homepage = HomePageObject(self.driver)
-        homepage.shop_button_click()
-        prod = "//div[@class='card h-100']/div"
-        products = self.driver.find_elements_by_xpath(f"{prod}/h4")
-        # self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        for product in products:
-            if product.text == "Blackberry":
-                product.find_element_by_xpath(f"{prod}/button").click()
+        homepage = HomePageObject(self.driver, self.wait)
+        page_page = homepage.shop_button_click()
+        prod_list = page_page.prod_buttons_list()
+        checkout = page_page.concr_prod_click(prod_list)
+        checkout.click_on_checkout_button()
+        checkout.click_on_success_button()
+        checkout.send_keys_to_country("Uni")
+        checkout.wait_for_element("United Kingdom")
+        checkout.checkbox_purchase()
 
-        self.driver.find_element_by_partial_link_text("Checkout").click()
-        self.driver.find_element_by_css_selector("button[class='btn btn-success']").click()
-        self.driver.find_element_by_id("country").send_keys("Uni")
-        self.wait.until(EC.presence_of_element_located((By.LINK_TEXT, "United Kingdom")))
-
-        self.driver.find_element_by_link_text("United Kingdom").click()
-        self.driver.find_element_by_class_name("checkbox-primary").click()
-        self.driver.find_element_by_css_selector("input[value='Purchase']").click()
         txt = self.driver.find_element_by_css_selector("div[class='alert alert-success alert-dismissible']").text
         assert "Success! Thank you!" in txt
 
